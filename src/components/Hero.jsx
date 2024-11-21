be921c2b5350cd1d491fc00334ab9d924609a8ef
kachman-git <onyekachiokpala67@gmail.com>
2024-11-21 16:19:56 +0000

    First Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
   return (
     <>
-      <section className="bg-[url(img/header.jpg)] header bg-no-repeat w-full  object-center bg-cover h-[90vh]">
+      <section className="header bg-no-repeat w-full  object-center bg-cover h-[90vh]">
         <div className="container relative flex flex-col items-center justify-center w-full h-full px-8 mx-auto text-center space-y-7">
           <h4 className="font-bold text-green-400 uppercase text-md">
             try up to one month free
